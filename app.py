a9cff71c4b3b32a9078be824a1e48557052979fc
Kusuma Yallapu <yallapukusuma7971@gmail.com>
2025-07-28 10:28:49 +0530

    The web application is ready.

--- app.py
+++ app.py
@@ -22,6 +22,18 @@ def predict_api():
     print(output[0])
     return jsonify(int((output[0])))
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=np.array(data).reshape(1,-1)
+    print(final_input)
+    output=rfcmodel.predict(final_input)[0]
+    messages = {
+        0: "No PCOS detected. Maintain a healthy lifestyle, including a balanced diet and regular exercise, to reduce the risk of developing PCOS.",
+        1: "You are likely to have PCOS. Please consult a doctor for further evaluation and guidance. In the meantime, consider making lifestyle changes such as eating a balanced diet, exercising regularly, and managing stress to help manage symptoms."
+    }
+
+    return render_template("home.html",prediction_text=messages[output])
 
 
 
